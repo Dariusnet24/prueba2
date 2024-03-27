9b69bbded6e61e603c9414ed8ca80dcc5984e24d
dariusnet24 <dariusnet24@gmail.com>
2024-03-26 23:59:25 -0400

    cambio git

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json()); 
 app.use(cors());
 
-app.use(`/api/Ussers`, userRouter);
+app.use(`/api/Users`, userRouter);
 app.use(`/api/Products`, productsRouter);
 app.use(`/api/Auth`, authRouter);
 
